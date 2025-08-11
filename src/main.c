2d01caa652cfd94b93121bf533c0c83cf66a0205
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-08-11 18:34:59 -0400

    source files updated

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
-void main()
+void main(void)
 {
+	//Initialize Game Manager
+	////////////////////////
+	
+	//Game Loop
+	while(true)
+	{
+		//Update Game Manager
+		/////////////
+		
+		end_frame();
+	}
 
+	//Deinitialize Game Manager
+	///////////////
 }

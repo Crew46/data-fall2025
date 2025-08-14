29f8d5e74dbc6c2271df57fc628a35f9f3ede668
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-08-14 18:03:39 -0400

    player header declarations added

--- src/player.c
+++ src/player.c
@@ -1,1 +1,28 @@
+#include "player.h"
 
+Player* CreatePlayer()
+{
+    return (Player*)malloc(sizeof(Player));
+}
+
+void InitializePlayer(Player* player, int x, int y, int health)
+{
+    player->x = x;
+    player->y = y;
+    player->health = health;
+}
+
+void DeInitializePlayer(Player* player)
+{
+    free(player);
+}
+
+void UpdatePlayer(Player* player)
+{
+
+}
+
+void DrawPlayer(Player* player)
+{
+
+}

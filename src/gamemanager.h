9fe5842696e64dfcdb8ea760b7d7ecebe4b6d768
Matthew Haas <haas@corning-cc.edu>
2025-08-14 11:47:28 -0400

    simple sample starter game logic

--- src/gamemanager.h
+++ src/gamemanager.h
@@ -1,6 +1,10 @@
 #ifndef GAMEMANAGER_H
 #define GAMEMANAGER_H
 
+#include "input.h"
+#include "misc.h"
+#include "string.h"
+#include "time.h"
 #include "video.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,17 +1,155 @@
 #include "gamemanager.h"
 
-void main(void)
+struct object  
 {
+	int region;
+	int x;
+	int y;
+	int alive;
+};
+
+void main (void)
+{
+	//int         chance              = 0;
+	int         index               = 0;
+	int         xpos                = 320;
+	int         ypos                = 300;
+	int         pewpews             = 0;
+	object [12] enemy;
+	object [4]  pewpew;
+
+	srand (get_cycle_counter ());
+
+	for (index = 0; index < 12; index++)
+	{
+		enemy[index].region         = 'v' - (rand () % 2 * 32);
+		enemy[index].x              = index * 57;
+		enemy[index].y              = 20 - (index * 20);
+		enemy[index].alive          = 1;
+	}
+
+	for (index = 0; index < 4; index++)
+	{
+		pewpew[index].region        = '.';
+		pewpew[index].x             = 0;
+		pewpew[index].y             = 0;
+		pewpew[index].alive         = 0;
+	}
+
 	//Initialize Game Manager
 	////////////////////////
+
+	select_gamepad (0);
+	select_texture (-1);
 	
 	//Game Loop
-	while(true)
+	while (true)
 	{
 		//Update Game Manager
 		/////////////
 		
-		//end_frame();
+		if (gamepad_left()         >  0)
+		{
+			xpos                    = xpos - 1;
+		}
+
+		if (gamepad_right()        >  0)
+		{
+			xpos                    = xpos + 1;
+		}
+
+		if (gamepad_up()           >  0)
+		{
+			ypos                    = ypos - 1;
+		}
+
+		if (gamepad_down()         >  0)
+		{
+			ypos                    = ypos + 1;
+		}
+
+		if (gamepad_button_a()     >  0)
+		{
+			if (pewpews            <  4)
+			{
+				pewpew[pewpews].x      = xpos;
+				pewpew[pewpews].y      = ypos;
+				pewpew[pewpews].alive  = 1;
+				pewpews                = pewpews + 1;
+			}
+		}
+
+		if (xpos                   <  0)
+		{
+			xpos                    = 0;
+		}
+
+		if (xpos                   >  630)
+		{
+			xpos                    = 630;
+		}
+
+		if (ypos                   >  350)
+		{
+			ypos                    = 350;
+		}
+
+		if (ypos                   <  0)
+		{
+			ypos                    = 0;
+		}
+
+		select_region ('^');
+		set_drawing_point (xpos, ypos);
+		draw_region ();
+
+		for (index = 0; index < 12; index++)
+		{
+			if (enemy[index].alive == 1)
+			{
+				select_region (enemy[index].region);
+				set_drawing_point (enemy[index].x, enemy[index].y);
+				draw_region ();
+
+				enemy[index].x      = rand () % 3 - 1;
+				enemy[index].y      = enemy[index].y + 1;
+
+				if (enemy[index].x <  0)
+				{
+					enemy[index].x  = 0;
+				}
+
+				if (enemy[index].x >  630)
+				{
+					enemy[index].x  = 630;
+				}
+
+				if (enemy[index].y >  630)
+				{
+					enemy[index].y  = rand () % 40 - 30;
+				}
+			}
+		}
+
+		if (pewpews                >  0)
+		{
+			for (index = 0; index < pewpews; index++)
+			{
+				select_region (pewpew[index].region);
+				set_drawing_point (pewpew[index].x, pewpew[index].y);
+				draw_region ();
+
+				pewpew[index].y     = pewpew[index].y - 1;
+
+				if (pewpew[index].y <  0)
+				{
+					pewpew[index].alive  = 0;
+					pewpews              = pewpews - 1;
+				}
+			}
+		}
+
+		end_frame ();
 	}
 
 	//Deinitialize Game Manager

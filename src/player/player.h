f12f7961b92a9b083ab81c80dd0490fc656823a3
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-08-21 15:30:14 -0400

    player.h comment

--- src/player/player.h
+++ src/player/player.h
@@ -4,6 +4,14 @@
 #include "../vector/vector2.h"
 #include "../laser/laser_controller.h"
 
+/// 
+/// This file is the model of player, containing player properties and functions
+///
+
+////////////////////////////////////////////////////////////
+///////////Player Struct////////////////////////////////////
+///////////////////////////////////////////////////////////
+
 struct Player 
 {
     int maxLasers; // Maximum number of lasers the player can have
@@ -13,25 +21,35 @@ struct Player
     float currentShootCooldownTime; //seconds
 };
 
-Player* CreatePlayer()
-{
-    return (Player*)malloc(sizeof(Player));
-}
+///////////////////////////////////////////////////////////
+///////////Constructor and Deconstructor///////////////////
+///////////////////////////////////////////////////////////
 
-void InitializePlayer(Player* player, int x, int y, float maxShootCooldownTime, int maxLasers)
+void CreatePlayer(int x, int y, float maxShootCooldownTime, int maxLasers)
 {
+    //allocate memory for player
+    Player* player = (Player*)malloc(sizeof(Player));
+
+    //player properties initialization
     player->maxLasers = maxLasers;
-    player->laser = (Laser*)malloc(sizeof(Laser) * maxLasers); // Initialize laser array
+    player->laser = (Laser*)malloc(sizeof(Laser) * maxLasers); // Initialize laser array of size maxLasers
     player->maxShootCooldownTime = maxShootCooldownTime;
     player->currentShootCooldownTime = 0; // Start with no cooldown
     player->position = CreateVector2(x, y);
 }
 
+//player deconstructor
 void DeinitializePlayer(Player* player)
 {
+    //free allocated heap memory in player struct
     free(player->position);
+    free(player->laser);
     free(player);
 }
+
+///////////////////////////////////////////////////////////
+///////////Player Functions////////////////////////////////
+///////////////////////////////////////////////////////////
 
 void MovePlayer(Player* player, Vector2* direction)
 {

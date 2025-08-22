7847d67c55101a7674574ccefaba631a0dd493eb
Darkjet21 <kbird2@corning-cc.edu>
2025-08-22 00:25:28 -0400

    background added

--- src/main.c
+++ src/main.c
@@ -1,22 +1,47 @@
 #include "game_manager.h"
 #include "time.h"
+#include "video.h"
 
-GameManager* gameManager;
+
+// Temporary spot to define stuff.
+	#define Background 0
+
+	GameManager* gameManager;
 
 void main(void)
 {
     // create and initialize the game manager
     gameManager = CreateGameManager();
+	
+
+
+// Getting the Background
+	select_texture( 0 );
+	select_region ( Background );
+	define_region_topleft( 0,0, 639,359);
+		
+
+	
+
+
+
 
     //game loop
     while(true)
     {
+		//drawing the background
+		select_texture ( 0 );
+		select_region ( Background );
+		draw_region_at( 0, 0 );
+
+
         //update game manager
         UpdateGameManager(gameManager);
+		
         end_frame();
     }
 
-    // Deinitialize game manager before exiting
-    DeinitializeGameManager(gameManager);
+	//Deinitialize game manager before exiting
+	DeinitializeGameManager(gameManager);
 }
 

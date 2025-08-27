1755a956df8a296131faeca9546670c5c76e3f58
Matthew Haas <haas@corning-cc.edu>
2025-08-27 08:33:13 -0400

    further cleaned up, commented main.c

--- src/main.c
+++ src/main.c
@@ -1,77 +1,126 @@
-#include "game_manager.h"
+#include "misc.h"
+#include "video.h"
 #include "time.h"
 
-GameManager* gameManager;
+#define  BACKGROUND_REGION 0
+#define  PLAYER_REGION     1
+
+struct Object
+{
+    int     textureID;
+    int     regionID;
+    int     id;
+    int     x;
+    int     y;
+    int     xdir;
+    int     ydir;
+    bool    isActive;
+    int     speed;
+    Object *next;
+};
 
 void main (void)
 {
-    // create and initialize the game manager
-//    gameManager     = CreateGameManager ();
-    Object *player  = (Object *) malloc (sizeof (Object) * 1); 
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Create our player instance
+    //
+    Object *player       = (Object *) malloc (sizeof (Object) * 1); 
+    player -> next       = NULL;
+    player -> x          = 360;
+    player -> y          = 300;
+    player -> textureID  = 1;
+    player -> regionID   = 1;
 
-    // Getting the Background
-    select_texture( BACKGROUND_TEXTURE );
-    select_region ( BACKGROUND_REGION );
-    define_region_topleft( 0,0, 639,359);
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Define the background texture and region
+    //
+    select_texture (BACKGROUND_TEXTURE);
+    select_region (BACKGROUND_REGION);
+    define_region_topleft (0, 0, 639, 359);
     
-    //player
-    select_texture(PLAYER_SPRITES_TEXTURE);
-    select_region(PLAYER_REGION);
-    define_region(0, 0, 31, 31, 0, 0); // Player sprite
-									   //
-	player -> x     = 360;
-	player -> y     = 300;
-	player -> textureID  = 1;
-	player -> regionID   = 1;
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Define the player texture and region
+    //
+    select_texture (PLAYER_SPRITES_TEXTURE);
+    select_region (PLAYER_REGION);
+    define_region (0, 0, 31, 31, 0, 0);
 
-    //game loop
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Select the first gamepad
+    //
+    select_gamepad (0);
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Game loop
+    //
     while (true)
     {
-        //clear screen
+        //clear screen -- do we really need this?
         clear_screen (get_color_red (0));    
 
-        //drawing the background
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Draw the background
+        //
         select_texture (BACKGROUND_TEXTURE);
         select_region (BACKGROUND_REGION);
         draw_region_at (0, 0);
 
-        //select the gamepad mapped to this player controller
-        select_gamepad (playerController -> gamepadID);
-        gamepad_direction (&player -> xdir, &player -> ydir); //get the direction from the gamepad
-        player -> x = player -> x + player -> xdir;
-        player -> y = player -> y + player -> ydir;
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Obtain directional information (per axis) from selected gamepad
+        //
+        gamepad_direction (&player -> xdir, &player -> ydir);
 
-        if (player -> x >  640)
-        {
-            player -> x  = 639;
-        }
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Adjust player position based on recently obtained gamepad information
+        //
+        player -> x      = player -> x + player -> xdir;
+        player -> y      = player -> y + player -> ydir;
 
-        if (player -> x <  0)
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Player/playfield bounds checking
+        //
+        if (player -> x <  0) // left side
         {
             player -> x  = 1;
         }
 
-        if (player -> y >  360)
+        if (player -> x >  640) // right side
         {
-            player -> y  = 359;
+            player -> x  = 639;
         }
 
-        if (player -> y <  0)
+        if (player -> y <  0) // top of screen
         {
             player -> y  = 1;
         }
-        
-        //select texture and region for first sprite in sprite array
-        select_texture (player -> textureID); // Select the player's texture
-        select_region (player -> regionID); // Select the player's sprite region
 
-        //draw the sprite at the player's position
-        draw_region_at (player -> x, player -> y); // Draw the sprite at the player's position
+        if (player -> y >  360) // bottom of screen
+        {
+            player -> y  = 359;
+        }
+        
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Select texture and region for the player
+        //
+        select_texture (player -> textureID);
+        select_region (player -> regionID);
+
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Draw the player the screen at is current x, y coordinates
+        //
+        draw_region_at (player -> x, player -> y);
 
         end_frame ();
     }
-
-    //Deinitialize game manager before exiting
-//    DeconstructGameManager (gameManager);
 }
-

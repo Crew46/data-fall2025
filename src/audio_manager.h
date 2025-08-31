6964ef3e06ef462bc31bff07dc85e4a7bfaf3bc4
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-08-31 18:43:54 -0400

    audio manager changes

--- src/audio_manager.h
+++ src/audio_manager.h
@@ -1,8 +1,26 @@
 #ifndef AUDIO_MANAGER_H
 #include AUDIO_MANAGER_H
+#include "misc.h"
+#include "audio.h"
 
-void PlaySound(int soundID)
+enum SoundEffect
 {
+    EXPLOSION_SOUND_EFFECT,
+    LASER_SOUND_EFFECT
+}
 
+void PlaySoundFx(SoundEffect soundEffect)
+{
+    //each sound effect has 4 variants, randomize from 0-3.
+    int randomValue = rand() % 4;
+    switch (soundEffect)
+    {
+    case EXPLOSION_SOUND_EFFECT:
+        break;
+    case LASER_SOUND_EFFECT:
+        break;
+    default:
+        break;
+    }
 }
 #endif //AUDIO_MANAGER_H

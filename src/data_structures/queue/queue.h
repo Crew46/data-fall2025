a67094d417632c6309715ded4dcc7ed8852d6cdc
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-09-24 19:17:39 -0400

    stack and queue implementing doubly linked list

--- src/data_structures/queue/queue.h
+++ src/data_structures/queue/queue.h
@@ -1,3 +1,10 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include "../doubly_linked_list/doubly_linked_list.h"
+
+struct Queue
+{
+    DoublyLinkedList* list;
+};
+
 #endif //QUEUE_H
--- src/data_structures/stack/stack.h
+++ src/data_structures/stack/stack.h
@@ -1,3 +1,10 @@
 #ifndef STACK_H
 #define STACK_H
+#include "../doubly_linked_list/doubly_linked_list.h"
+
+struct Stack
+{
+    DoublyLinkedList* list;
+};
+
 #endif //STACK_H

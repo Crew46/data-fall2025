6e79654d31083c4c6443f3241943ada686f0c225
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-09-24 19:23:46 -0400

    stack declarations: Unstack, Peak, Stack. Queue Declarations: Enqueue, Dequeue, Peak

--- src/data_structures/queue/queue.h
+++ src/data_structures/queue/queue.h
@@ -7,4 +7,13 @@ struct Queue
     DoublyLinkedList* list;
 };
 
+//add node to queue
+void Enqueue(DoublyNode* doublyNode);
+
+//remove node from queue and return it
+DoublyNode* Dequeue();
+
+//peaks to see the next node in queue
+DoublyNode* Peak();
+
 #endif //QUEUE_H
--- src/data_structures/stack/stack.h
+++ src/data_structures/stack/stack.h
@@ -1,10 +1,20 @@
 #ifndef STACK_H
 #define STACK_H
 #include "../doubly_linked_list/doubly_linked_list.h"
+#include "../doubly_linked_list/doubly_node.h"
 
 struct Stack
 {
     DoublyLinkedList* list;
 };
+
+//add node to stack
+void Stack(DoublyNode* doublyNode);
+
+//remove node from stack and return it
+DoublyNode* Unstack();
+
+//see the next node in the stack
+DoublyNode* Peak();
 
 #endif //STACK_H

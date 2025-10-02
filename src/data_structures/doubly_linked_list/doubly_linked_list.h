eb1385822270477b555f7cc185ecba0e7a0583f5
t0mmyka <tkastne1@corning-cc.edu>
2025-10-02 15:04:18 -0400

    Moved unified list and node into folder

--- src/data_structures/doubly_linked_list/doubly_linked_list.h
+++ src/data_structures/doubly_linked_list/doubly_linked_list.h
@@ -1,1 +1,190 @@
-#include "../../list.h"
+#ifndef _LIST_H
+#define _LIST_H
+
+#include "node.h"
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  Doubly Linked List struct and helpers
+//
+struct List
+{
+    Node *head;
+    Node *tail;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  append(): add a newNode into myList after indicated place
+//
+List *append (List *myList, Node *place, Node *newNode)
+{
+    if (myList              == NULL)
+    {
+        myList               = (List *) malloc (sizeof (List));
+    }
+
+    if ((myList             != NULL) &&
+        (newNode            != NULL))
+    {
+        Node *tmp          = NULL;
+        Node *tmp2         = NULL;
+
+        if (myList -> head  == NULL)
+        {
+            myList -> head   = newNode;
+            myList -> tail   = newNode;
+        }
+        else if (place      == myList -> tail)
+        {
+            tmp              = myList -> tail;
+            tmp -> next      = newNode;
+            newNode -> prev  = tmp;
+            myList -> tail   = newNode;
+        }
+        else
+        {
+            tmp              = place -> next;
+            tmp -> prev      = newNode;
+            newNode -> prev  = place;
+            place -> next    = newNode;
+            newNode -> next  = tmp;
+        }
+    }
+
+    return (myList);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  insert(): add a newNode into myList before indicated place
+//
+List *insert (List *myList, Node *place, Node *newNode)
+{
+    if (myList              == NULL)
+    {
+        myList               = (List *) malloc (sizeof (List));
+    }
+
+    if ((myList             != NULL) &&
+        (newNode            != NULL))
+    {
+        Node *tmp          = NULL;
+        Node *tmp2         = NULL;
+
+        if (myList -> head  == NULL)
+        {
+            myList -> head   = newNode;
+            myList -> tail   = newNode;
+        }
+        else if (place      == myList -> head)
+        {
+            tmp              = myList -> head;
+            tmp -> prev      = newNode;
+            newNode -> next  = tmp;
+            myList -> head   = newNode;
+        }
+        else
+        {
+            tmp              = place -> prev;
+            tmp -> next      = newNode;
+            newNode -> next  = place;
+            place -> prev    = newNode;
+            newNode -> prev  = tmp;
+        }
+    }
+
+    return (myList);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  obtain(): take thatNode and disconnect it (while maintaining list integrity)
+//            from myList.
+//
+List *obtain (List *myList, Node *thatNode)
+{
+    if (!myList || !thatNode || !(thatNode))
+        return (myList);
+
+    // Only one node
+    if (myList -> head           == thatNode &&
+        myList -> tail           == thatNode)
+    {
+        myList -> head            = NULL;
+        myList -> tail            = NULL;
+    }
+    else if (thatNode            == myList -> head)
+    {
+        myList -> head            = thatNode -> next;
+        myList -> head -> prev    = NULL;
+    }
+    else if (thatNode            == myList -> tail)
+    {
+        myList -> tail            = thatNode -> prev;
+        myList -> tail -> next    = NULL;
+    }
+    else
+    {
+        thatNode -> next -> prev  = thatNode -> prev;
+        thatNode -> prev -> next  = thatNode -> next;
+    }
+
+    return (myList);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  createList(): allocate and initialize a new list
+//
+List *createList ()
+{
+    List *myList        = (List*) malloc (sizeof (List));
+    if (myList         != NULL)
+    {
+        myList -> head  = NULL;
+        myList -> tail  = NULL;
+    }
+
+    return (myList);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  clearList(): sanely obtain and deallocate all nodes in myList, leaving an
+//               empty list
+//
+List *clearList (List *myList)
+{
+    Node *tmp         = NULL;
+    if (myList       != NULL)
+    {
+        tmp           = myList -> head;
+        while (tmp   != NULL)
+        {
+            myList    = obtain (myList, tmp);
+            tmp       = deleteNode (tmp);
+            tmp       = myList -> head;
+        }
+    }
+
+    return (myList);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  deleteList(): clear and deallocate a list, returning a NULL
+//
+List *deleteList (List *oldList)
+{
+    if (oldList      != NULL)
+    {
+        oldList       = clearList (oldList);
+        free (oldList);
+        oldList       = NULL;
+    }
+
+    return (oldList);
+}
+
+#endif
--- src/data_structures/doubly_linked_list/doubly_node.h
+++ src/data_structures/doubly_linked_list/doubly_node.h
@@ -1,1 +1,37 @@
-#include "../../node.h"
+#ifndef _NODE_H
+#define _NODE_H
+#include "object.h"
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  Node struct and helpers
+//
+struct Node
+{
+	Object *data;
+	Node   *next;
+	Node   *prev;
+};
+
+Node *createNode (Object *data)
+{
+	Node *node    = (Node *) malloc (sizeof (Node));
+	node -> data  = data;
+	node -> next  = NULL;
+	node -> prev  = NULL;
+
+	return node;
+}
+
+Node *deleteNode (Node *oldNode)
+{
+	if (oldNode  != NULL)
+	{
+		free (oldNode);
+		oldNode   = NULL;
+	}
+
+	return (oldNode);
+}
+
+#endif

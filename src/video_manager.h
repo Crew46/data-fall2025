8321d9ea1d7c8484de7d03d61f273c3505614e0e
Matthew Haas <haas@corning-cc.edu>
2025-10-14 10:45:43 -0400

    reviewing code, adding some initializations

--- src/video_manager.h
+++ src/video_manager.h
@@ -1,34 +1,34 @@
-#ifndef  VIDEO_MANAGER_H
-#define  VIDEO_MANAGER_H
+#ifndef  _VIDEO_MANAGER_H
+#define  _VIDEO_MANAGER_H
 #include "video.h"
 #include "object.h"
 #include "data_structures/doubly_linked_list/doubly_linked_list.h"
 #include "data_structures/queue/queue.h"
 #include "data_structures/stack/stack.h"
 
-void drawList(List* list)
+void drawList (List *list)
 {
-    Node *currentNode = list->head;
-    Node *nextNode;
+    Node *currentNode   = list -> head;
+    Node *nextNode      = NULL;
 
-    while(currentNode != NULL)
+    while (currentNode != NULL)
     {
-        nextNode = currentNode->next;
+        nextNode        = currentNode -> next;
 
-        drawObject(currentNode->data);
+        drawObject (currentNode -> data);
 
-        currentNode = nextNode;
+        currentNode     = nextNode;
     }
 }
 
-void drawQueue(Queue* queue)
+void drawQueue (Queue *queue)
 {
-    drawList(queue->list);
+    drawList (queue -> list);
 }
 
-void drawStack(Stack* stack)
+void drawStack (Stack *stack)
 {
-    drawList(stack->list);
+    drawList (stack -> list);
 }
 
-#endif
+#endif // _VIDEO_MANAGER_H

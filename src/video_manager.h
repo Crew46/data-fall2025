859ec782b8a5c35d46deebcf0bb1c5d4ebf4bb2d
t0mmyka <tkastne1@corning-cc.edu>
2025-10-14 10:20:16 -0400

    Forgot to add video manager

---
+++ src/video_manager.h
@@ -0,0 +1,34 @@
+#ifndef  VIDEO_MANAGER_H
+#define  VIDEO_MANAGER_H
+#include "video.h"
+#include "object.h"
+#include "data_structures/doubly_linked_list/doubly_linked_list.h"
+#include "data_structures/queue/queue.h"
+#include "data_structures/stack/stack.h"
+
+void drawList(List* list)
+{
+    Node *currentNode = list->head;
+    Node *nextNode;
+
+    while(currentNode != NULL)
+    {
+        nextNode = currentNode->next;
+
+        drawObject(currentNode->data);
+
+        currentNode = nextNode;
+    }
+}
+
+void drawQueue(Queue* queue)
+{
+    drawList(queue->list);
+}
+
+void drawStack(Stack* stack)
+{
+    drawList(stack->list);
+}
+
+#endif

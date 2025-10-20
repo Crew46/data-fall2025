912a0b6e117c953f053af24943424ca6229dd366
Matthew Haas <haas@corning-cc.edu>
2025-10-20 17:12:12 -0400

    testing the moving of some audio logic to the audio_manager, prototyping some audio triggering code

--- src/audio_manager.h
+++ src/audio_manager.h
@@ -3,6 +3,82 @@
 #include "misc.h"
 #include "audio.h"
 
+/*
+struct AudioTrigger
+{
+    int position;
+    int vy_obj_factor;
+    int max_obj_vy;
+    int min_obj_vy;
+};
+
+struct AudioAction
+{
+    int           num_triggers; // number of triggers
+    AudioTrigger *segment;      // array containing the AudioTriggers for a track
+};
+
+    AudioAction [6] AudioOffset;
+    AudioOffset[0].num_triggers              = 4;
+    AudioOffset[0].segment                   = (AudioTrigger *) malloc (sizeof (AudioTrigger) * 4);
+    AudioOffset[0].segment[0].position       = 220000;
+    AudioOffset[0].segment[0].vy_obj_factor  = 0;
+    AudioOffset[0].segment[0].max_obj_vy     = -1000;
+    AudioOffset[0].segment[0].min_obj_vy     = -1000;
+    AudioOffset[0].segment[1].position       = 350000;
+    AudioOffset[0].segment[1].vy_obj_factor  = 1;
+    AudioOffset[0].segment[1].max_obj_vy     = -1000;
+    AudioOffset[0].segment[1].min_obj_vy     = -1000;
+    AudioOffset[0].segment[2].position       = 575000;
+    AudioOffset[0].segment[2].vy_obj_factor  = 1;
+    AudioOffset[0].segment[2].max_obj_vy     = 4;
+    AudioOffset[0].segment[2].min_obj_vy     = 1;
+    AudioOffset[0].segment[3].position       = -1000;
+    AudioOffset[0].segment[3].vy_obj_factor  = 2;
+    AudioOffset[0].segment[3].max_obj_vy     = -1000;
+    AudioOffset[0].segment[3].min_obj_vy     = -1000;
+    AudioOffset[1].num_triggers              = 4;
+    AudioOffset[1].segment                   = (AudioTrigger *) malloc (sizeof (AudioTrigger) * 4);
+    AudioOffset[1].segment[0].position       = 100000;
+    AudioOffset[1].segment[0].vy_obj_factor  = 0;
+    AudioOffset[1].segment[0].max_obj_vy     = -1000;
+    AudioOffset[1].segment[0].min_obj_vy     = -1000;
+    AudioOffset[1].segment[1].position       = 315000;
+    AudioOffset[1].segment[1].vy_obj_factor  = 1;
+    AudioOffset[1].segment[1].max_obj_vy     = -1000;
+    AudioOffset[1].segment[1].min_obj_vy     = -1000;
+    AudioOffset[1].segment[2].position       = 600000;
+    AudioOffset[1].segment[2].vy_obj_factor  = 1;
+    AudioOffset[1].segment[2].max_obj_vy     = 4;
+    AudioOffset[1].segment[2].min_obj_vy     = 1;
+    AudioOffset[1].segment[3].position       = -1000;
+    AudioOffset[1].segment[3].vy_obj_factor  = 2;
+    AudioOffset[1].segment[2].max_obj_vy     = -1000;
+    AudioOffset[1].segment[2].min_obj_vy     = -1000;
+
+    position                   = get_channel_position (0);
+    for (index = 0; index < AudioOffset[currentState].num_triggers; index++)
+    {
+        if (position          <  AudioOffset[currentState].segment[index].position;
+        {
+            if (AudioOffset[currentState].segment[index].vy_obj_factor >  -1000)
+            {
+                vy_obj_factor  = AudioOffset[currentState].segment[index].vy_obj_factor;
+            }
+
+            if (AudioOffset[currentState].segment[index].max_obj_vy >  -1000)
+            {
+                max_obj_vy     = AudioOffset[currentState].segment[index].max_obj_vy;
+            }
+
+            if (AudioOffset[currentState].segment[index].min_obj_vy >  -1000)
+            {
+                min_obj_vy     = AudioOffset[currentState].segment[index].min_obj_vy;
+            }
+        }
+    }
+*/
+
 enum SoundEffect
 {
     EXPLOSION_SOUND_EFFECT,

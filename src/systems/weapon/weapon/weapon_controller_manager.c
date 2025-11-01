934b25bbb0b6eccfb4899b253061ee06a2986f13
Tyler Strickland <tylerstricklandwork@gmail.com>
2025-11-01 14:01:09 -0400

    weapon controller initialize and construct

--- src/systems/weapon/weapon/weapon_controller_manager.c
+++ src/systems/weapon/weapon/weapon_controller_manager.c
@@ -1,10 +1,12 @@
 #ifndef WEAPON_CONTROLLER_MANAGER_C
 #define WEAPON_CONTROLLER_MANAGER_C
 #include "weapon_controller_manager.h"
+#include "../../../architecture/component/component_manager.h"
 
 void WCM_WeaponControllerInitialize(WeaponController* controller)
 {
-
+    CM_InitializeComponent((Component*)controller, WEAPON_CONTROLLER_COMPONENT);
+    WCM_WeaponControllerSet_DescriptiveData(controller, WEAPON_CONTROLLER_TYPE_LASER_CANNON);
 }
 
 WeaponController* WCM_WeaponControllerConstruct()
